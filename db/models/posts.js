8bf214e02468f637b35ef4884cbc8dc369e22640
Kevin Huang <kevinzhuang10@gmail.com>
2017-09-01 14:02:35 -0700

    Add database methods

--- db/models/posts.js
+++ db/models/posts.js
@@ -3,6 +3,20 @@ const Post = db.Model.extend({
   tableName: 'posts',
   profile: function() {
     return this.belongsTo('Profile');
+  },
+
+  createPost: function(profile_id, lat, lng, content, title) {
+    return this.forge({
+      profile_id,
+      lat,
+      lng,
+      content,
+      title
+    }).save();
+  },
+
+  getPostById: function(id) {
+    return this.where('id', id).fetch();
   }
 });
 
