d9c59618161589d7a32c71e85779ac51cb7cbce3
Kevin Huang <kevinzhuang10@gmail.com>
2017-09-02 15:37:24 -0700

    remove extra files

--- client/src/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestContainer from './components/testContainer.jsx';
-
-ReactDOM.render(<TestContainer />, document.getElementById('root'));
