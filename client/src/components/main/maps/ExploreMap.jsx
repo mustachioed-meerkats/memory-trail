b2bb81353c362d16cb5396ee68c9b3f8fbfbc30d
Kevin Huang <kevinzhuang10@gmail.com>
2017-09-03 11:56:54 -0700

    add searchbox

--- client/src/components/main/maps/ExploreMap.jsx
+++ client/src/components/main/maps/ExploreMap.jsx
@@ -2,22 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {withGoogleMap, GoogleMap, Marker} from 'react-google-maps';
+import SearchBox from 'react-google-maps/lib/places/SearchBox';
 
 import Markers from './Markers.jsx';
 import Search from './Search.jsx';
 import PostList from '../PostList.jsx';
 
+const INPUT_STYLE = {
+  boxSizing: `border-box`,
+  MozBoxSizing: `border-box`,
+  border: `1px solid transparent`,
+  width: `240px`,
+  height: `32px`,
+  marginTop: `27px`,
+  padding: `0 12px`,
+  borderRadius: `1px`,
+  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+  fontSize: `14px`,
+  outline: `none`,
+  textOverflow: `ellipses`,
+};
+
 const GettingStartedGoogleMap = withGoogleMap(props => (
   <GoogleMap
-    ref={props.onMapLoad}
+    ref={props.onMapMounted}
     defaultZoom={12}
     center={props.mapCenter}
     onClick={props.onMapClick}
+    onBoundsChanged={props.onBoundsChanged}
   >
+    <SearchBox
+      ref={props.onSearchBoxMounted}
+      bounds={props.bounds}
+      controlPosition={google.maps.ControlPosition.TOP_LEFT}
+      onPlacesChanged={props.onPlacesChanged}
+      inputPlaceholder='Search for a place!'
+      inputStyle={INPUT_STYLE}
+    />
     {props.markers.map(marker => (
       <Marker
         {...marker}
-        onRightClick={() => props.onMarkerRightClick(marker)}
       />
     ))}
   </GoogleMap>
@@ -34,18 +58,22 @@ class ExploreMap extends React.Component {
         },
         key: 'Golden Gate Park',
         defaultAnimation: 2
-      }]
+      }],
+      bounds: null,
+      center: {
+        lat: 47.6205588,
+        lng: -122.3212725,
+      }
     };
-    this.handleMapLoad = this.handleMapLoad.bind(this);
+    this.handleMapMounted = this.handleMapMounted.bind(this);
     this.handleMapClick = this.handleMapClick.bind(this);
-    this.handleMarkerRightClick = this.handleMarkerRightClick.bind(this);
+    this.handleBoundsChanged = this.handleBoundsChanged.bind(this);
+    this.handleSearchBoxMounted = this.handleSearchBoxMounted.bind(this);
+    this.handlePlacesChanged = this.handlePlacesChanged.bind(this);
   }
 
-  handleMapLoad(map) {
+  handleMapMounted(map) {
     this._mapComponent = map;
-    if (map) {
-      console.log(map.getZoom());
-    }
   }
 
   handleMapClick(event) {
@@ -62,10 +90,29 @@ class ExploreMap extends React.Component {
     });
   }
 
-  handleMarkerRightClick(targetMarker) {
-    const nextMarkers = this.state.markers.filter(marker => marker !== targetMarker);
+  handleBoundsChanged() {
     this.setState({
-      markers: nextMarkers
+      bounds: this._mapComponent._map.getBounds(),
+      center: this._map.getCenter()
+    });
+  }
+
+  handleSearchBoxMounted(searchBox) {
+    this._searchBox = searchBox;
+  }
+
+  handlePlacesChanged() {
+    const places = this._searchBox.getPlaces();
+    console.log('got some new places: ', places);
+    const markers = places.map(place => ({
+      position: place.geometry.location
+    }));
+
+    const mapCenter = markers.length > 0 ? markers[0].position : this.state.center;
+
+    this.setState({
+      center: mapCenter,
+      markers
     });
   }
 
@@ -73,17 +120,16 @@ class ExploreMap extends React.Component {
     return (
       <div style={{height: '700px'}}>
         <GettingStartedGoogleMap
-          containerElement={
-            <div style={{height: '100%'}} />
-          }
-          mapElement={
-            <div style={{height: '100%'}} />
-          }
-          mapCenter={this.props.mapCenter}
-          onMapLoad={this.handleMapLoad}
+          containerElement={<div style={{height: '100%'}} />}
+          mapElement={<div style={{height: '100%'}} />}
+          mapCenter={this.state.center}
+          onMapMounted={this.handleMapMounted}
           onMapClick={this.handleMapClick}
           markers={this.state.markers}
-          onMarkerRightClick={this.handleMarkerRightClick}
+          onBoundsChanged={this.onBoundsChanged}
+          onSearchBoxMounted={this.handleSearchBoxMounted}
+          bounds={this.state.bounds}
+          onPlacesChanged={this.handlePlacesChanged}
         />
       </div>
     );
