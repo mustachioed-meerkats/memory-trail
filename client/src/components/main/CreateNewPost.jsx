40b2ad4bdb24771017e88a31a7fcc620471d6278
dylan <dylan@dylans-MacBook-Pro.local>
2017-09-05 14:23:00 -0700

    Latest version of location search

--- client/src/components/main/CreateNewPost.jsx
+++ client/src/components/main/CreateNewPost.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import Autocomplete, { onPlaceSelected } from 'react-google-autocomplete';
+import Autocomplete from 'react-google-autocomplete';
 
 
 /** ============================================================
@@ -27,9 +27,7 @@ import {
 const CreateNewPost = (props) => {
 
   // Autocomplete feature for the form's location input field
-  const initLocationAutocomplete = (e) => {
-    e.persist();
-    let address = e.target.value;
+  const initLocationAutocomplete = () => {
     let input = document.getElementById('locationInput');    
     let autocomplete = new google.maps.places.Autocomplete(input);
   }
@@ -41,10 +39,12 @@ const CreateNewPost = (props) => {
       console.log('response from geocoding API: ', res);
       props.handleLocationInput(res.data.results[0].geometry.location);
     })
+    .catch((err) => {
+      console.log('(Client) Error calling Google Geocoding API');
+    });
   }
 
   const handleSubmit = () => {
-    console.log('value of props.location: ', props.location);
     const post = {
       title: props.title,
       content: props.content,
@@ -81,14 +81,20 @@ const CreateNewPost = (props) => {
               />
             </FormGroup>
             <FormGroup>
+              <div> Use current location for post? </div>
+              {/* <ButtonToolbar>
+                <Button bsSize="sm" onClick={props.handleLocationInput(props.map)}> Yes </Button>
+                <Button bsSize="sm"> Choose a location </Button>
+              </ButtonToolbar> */}
               <Autocomplete
                 id="locationInput"
-                onChange={(e) => initLocationAutocomplete(e)}
+                style={{width: '100%'}}
+                onChange={initLocationAutocomplete}
                 onPlaceSelected={(place) => {
                   geocodeLocationInput(place.name);
                 }}
               />
-              
+
               {/* <FormControl
               type="text"
               id="locationInput"
