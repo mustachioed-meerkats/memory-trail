a39a03e57de1db88842918d51ff7d5361c939695
Samuel Davis <davisss64@gmail.com>
2017-09-08 12:23:00 -0700

    Refactored into stateful component.

--- client/src/components/main/CreateNewPost.jsx
+++ client/src/components/main/CreateNewPost.jsx
@@ -22,112 +22,115 @@ import {
   handleLocationInput
 } from '../../store/modules/newpost';
 
-const CreateNewPost = (props) => {
-
-  const geocodeLocationInput = (location) => {
-    // calls google geocoding API to fetch lat/lng from address selected in autocomplete form
-    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyDXLOMgs19AOUHeizaMnRwjVyzxcTGWmJ8`;
-    return axios.get(url)
-      .then((res) => {
-        console.log('response from geocoding API: ', res);
-        // action handler to update location value in state
-        props.handleLocationInput(res.data.results[0].geometry.location);
-      })
-      .catch((err) => {
-        console.log('(Client) Error calling Google Geocoding API');
-      });
-  };
-
-  // Autocomplete feature for the form's location input field
-  const initializeAutocomplete = () => {
-    let input = document.getElementById('locationInput');
-    // render predictions from google autocomplete using input from location field
-    let autocomplete = new google.maps.places.Autocomplete(input);
-    // listen for location selection from the dropdown
-    let place;
-    google.maps.event.addListener(autocomplete, 'place_changed', () => {
-      place = autocomplete.getPlace();
-      console.log(place);
-      // when a place is selected, use its address property to call google geocoding API
-      geocodeLocationInput(place.formatted_address);
-    });
-  };
-
-  const handleSubmit = () => {
-    let post = {
-      title: props.title,
-      content: props.content,
-      lat: props.location.lat,
-      lng: props.location.lng,
-      profile_id: props.user.id
+class CreateNewPost extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      show: false,
+    };
+    const geocodeLocationInput = (location) => {
+      // calls google geocoding API to fetch lat/lng from address selected in autocomplete form
+      let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyDXLOMgs19AOUHeizaMnRwjVyzxcTGWmJ8`;
+      return axios.get(url)
+        .then((res) => {
+          console.log('response from geocoding API: ', res);
+          // action handler to update location value in state
+          this.props.handleLocationInput(res.data.results[0].geometry.location);
+        })
+        .catch((err) => {
+          console.log('(Client) Error calling Google Geocoding API');
+        });
     };
 
-    console.log('(Client) Intiating POST Request! CREATING NEW POST');
-    
-    return axios.post('/api/posts/new', post)
-      .then(result => {
-        console.log('(Client) Success! CREATING NEW POST');
-        // TODO: Add redirection to Explore Map
-      })
-      .catch((err) => {
-        console.log('(Client) Error! CREATING NEW POST');
-        console.log(err);
+    // Autocomplete feature for the form's location input field
+    const initializeAutocomplete = () => {
+      let input = document.getElementById('locationInput');
+      // render predictions from google autocomplete using input from location field
+      let autocomplete = new google.maps.places.Autocomplete(input);
+      let place;
+      // listen for location selection from the dropdown
+      google.maps.event.addListener(autocomplete, 'place_changed', () => {
+        place = autocomplete.getPlace();
+        // when a place is selected, use its address property to call google geocoding API
+        geocodeLocationInput(place.formatted_address);
       });
-  };
+    };
 
-  return (
-    <Grid>
-      <Row>
-        <Col sm={4}>
-        </Col>
-        <Col sm={4}>
-          <form>
-            <FormGroup>
-              <FormControl
-                type="text" 
-                onChange={(e) => { props.handleTitleInput(e.target.value); }}
-                placeholder="Title"
-              />
-            </FormGroup>
-            <FormGroup>
-              <Autocomplete
-                className="form-control"
-                id="locationInput"
-                placeholder="Search for places"
-                style={{width: '100%'}}
-                onChange={initializeAutocomplete}
-              />
-            </FormGroup>
-            <FormGroup>
-              <FormControl
-                componentClass="textarea"
-                onChange={(e) => { props.handleContentTextArea(e.target.value); }}
-                placeholder="Memory..."
-              />
-            </FormGroup>
-          </form>
-        </Col>
-        <Col sm={4}>
-        </Col>
-      </Row>
-      <Row>
-        <Upload/>
-      </Row>
-      <Row>
-        <Col sm={4}>
-        </Col>
-        <Col sm={4}>
-          <ButtonToolbar style={{textAlign: 'center'}}>
-            <Button type="submit" bsStyle="success" onClick={handleSubmit}>Publish</Button>
-            <Button href="/" bsStyle="danger">Cancel</Button>
-          </ButtonToolbar>
-        </Col>
-        <Col sm={4}>
-        </Col>
-      </Row>
-    </Grid>
-  );
-};
+    const handleSubmit = () => {
+      const post = {
+        title: this.props.title,
+        content: this.props.content,
+        lat: this.props.location.lat,
+        lng: this.props.location.lng,
+        profile_id: this.props.user.id
+      };
+
+      console.log('(Client) Intiating POST Request! CREATING NEW POST');
+      console.log(post);
+      return axios.post('/api/posts/new', post)
+        .then(result => {
+          console.log('(Client) Success! CREATING NEW POST');
+          // TODO: Add redirection to Explore Map
+        })
+        .catch((err) => {
+          console.log('(Client) Error! CREATING NEW POST');
+          console.log(err);
+        });
+    };
+  }
+
+  render () {
+    return (
+      <Grid>
+        <Row>
+          <Col sm={4}>
+          </Col>
+          <Col sm={4}>
+            <form>
+              <FormGroup>
+                <FormControl
+                  type="text"
+                  onChange={(e) => { this.props.handleTitleInput(e.target.value); }}
+                  placeholder="Title"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Autocomplete
+                  className="form-control"
+                  id="locationInput"
+                  placeholder="Search for places"
+                  style={{width: '100%'}}
+                  onChange={this.initializeAutocomplete}
+                />
+              </FormGroup>
+              <FormGroup>
+                <FormControl
+                  componentClass="textarea"
+                  onChange={(e) => { this.props.handleContentTextArea(e.target.value); }}
+                  placeholder="Memory..."
+                />
+              </FormGroup>
+            </form>
+          </Col>
+          <Col sm={4}>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={4}>
+          </Col>
+          <Col sm={4}>
+            <ButtonToolbar style={{textAlign: 'center'}}>
+              <Button type="submit" bsStyle="success" onClick={this.handleSubmit}>Publish</Button>
+              <Button href="/" bsStyle="danger">Cancel</Button>
+            </ButtonToolbar>
+          </Col>
+          <Col sm={4}>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
 
 /** ============================================================
  * Define State Subscriptions
