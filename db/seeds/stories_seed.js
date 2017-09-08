e67001ed2af0097665584ede49987715eed088a5
Kevin Huang <kevinzhuang10@gmail.com>
2017-09-08 01:38:14 -0700

    modify seed file

--- db/seeds/stories_seed.js
+++ db/seeds/stories_seed.js
@@ -2,34 +2,22 @@ const models = require('../models');
 
 exports.seed = function (knex, Promise) {
 
-  return models.Profile.where({ email: 'admin@domain.com' }).fetch()
-    .then((profile) => {
-      if (profile) {
-        throw profile;
+  return models.Story.where({title: 'everyday life'}).fetch()
+    .then(story => {
+      if (story) {
+        throw story;
       }
-      return models.Profile.forge({
-        first: 'Natalie',
-        last: 'Garnett',
-        display: 'Natalie B Garnett',
-        email: 'nat@kev.com'
+      return models.Story.forge({
+        profile_id: 1,
+        title: 'everyday life',
+        summary: 'This is a test story. it is getting late.'
       }).save();
     })
     .error(err => {
       console.error('ERROR: failed to create profile');
       throw err;
     })
-    .then((profile) => {
-      return models.Auth.forge({
-        type: 'local',
-        password: 'nat123',
-        profile_id: profile.get('id')
-      }).save();
-    })
-    .error(err => {
-      console.error('ERROR: failed to create auth');
-    })
     .catch(() => {
       console.log('WARNING: defualt user already exists.');
     });
-
 };
