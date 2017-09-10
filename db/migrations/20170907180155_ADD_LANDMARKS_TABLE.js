3540a4ab9d50c34cfe54dc25fa87883bf5f47bfd
Kevin Huang <kevinzhuang10@gmail.com>
2017-09-10 16:17:49 -0700

    change landmarks and post schema

--- db/migrations/20170907180155_ADD_LANDMARKS_TABLE.js
+++ db/migrations/20170907180155_ADD_LANDMARKS_TABLE.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('landmarks', function(table) {
     table.increments('id').unsigned().primary();
     table.string('name', 100).notNullable();
-    table.text('description').notNullable();
-    table.text('image_url').notNullable();
+    table.text('description');
+    table.text('image_url');
     table.decimal('lat', 10, 8).notNullable();
     table.decimal('lng', 11, 8).notNullable();
   });
--- db/migrations/20170907182945_ADD_STORY_AND_LANDMARK_ID_TO_POST_TABLE.js
+++ db/migrations/20170907182945_ADD_STORY_AND_LANDMARK_ID_TO_POST_TABLE.js
@@ -3,11 +3,13 @@ exports.up = function(knex, Promise) {
   return knex.schema.table('posts', function(table) {
     table.integer('story_id').references('stories.id').onDelete('CASCADE');
     table.integer('landmark_id').references('landmarks.id').onDelete('CASCADE');
+    table.text('image_url');
   });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.table('posts', function(table) {
+    table.dropColumn('image_url');
     table.dropColumn('landmark_id');
     table.dropColumn('story_id');
   });
