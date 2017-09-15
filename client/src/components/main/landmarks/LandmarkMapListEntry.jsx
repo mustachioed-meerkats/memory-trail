f6680703ac6d15a912d1ada649561504bfbfaad2
Talis Lazdins <lazdinst@gmail.com>
2017-09-15 15:53:35 -0700

    Updated LandmarkMapListEntry with applicable data

--- client/src/components/main/landmarks/LandmarkMapListEntry.jsx
+++ client/src/components/main/landmarks/LandmarkMapListEntry.jsx
@@ -25,42 +25,54 @@ import {
   Loader
 } from 'semantic-ui-react';
 
-const style = {
-  card: {
-    margin: '-1rem -1rem 0.5rem -1rem',
-    height: '5rem',
-    background: 'url(http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1444253482/DG2015-san-francisco.jpg?itok=MdRJm2Zo)  center center',
-    backgroundSize: 'cover'
-  }
-};
-
 /** ============================================================
  * Define Component
  * ========================================================== */
-const LandmarkMapListListEntry = (props) => {
+const LandmarkMapListListEntry = ({landmark}) => {
+  const style = {
+    card: {
+      margin: '-1rem -1rem 0.5rem -1rem',
+      height: '5rem',
+      background: `url(${landmark.image_url})  center center`,
+      backgroundSize: 'cover'
+    }
+  };
+
   return (
     <Card raised={true}> 
       <Card.Content>
         <div style={style.card}>
         </div >
         <Card.Meta>
-          <div>
-            <a><Icon name='heart' /># </a>
-            <a><Icon name='map pin' /> LAND_MARK</a>
-          </div>
-          Submitted {getTimeSincePost('2017-09-12')} hrs ago by <strong>{'PROFILE_DISPLAY'}</strong>
         </Card.Meta>
         <Card.Description>
-          {'CONTENT CONTENT'}
+          <Link to={`/landmark/${landmark.id}`}>{landmark.name}</Link>
         </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button circular icon='heart' />
-        <Button circular icon='commenting outline' />
-        <Button circular icon='share alternate' />
       </Card.Content>
     </Card>
   );
 };
 
 export default LandmarkMapListListEntry;
+
+
+// id(pin): 1
+// name(pin): "Oakland"
+// image_url(pin): "https://lh3.googleusercontent.com/p/AF1QipMQneBzguRjSYMM-SQi_Fyrym80ek4EbXfx8u8S=w1080-k"
+// lat(pin): "37.80436370"
+// lng(pin): "-122.27111370"
+// google_id(pin): "ChIJA-2qKIt9hYARZ5N1NdUVtHE"
+// ▶posts(pin)
+// ▶0(pin)
+// id(pin): 1
+// profile_id(pin): 1
+// lat(pin): "37.80436370"
+// lng(pin): "-122.27111370"
+// content(pin): "This is a Memoery for my story"
+// title(pin): "Placeholder"
+// created_at(pin): "2017-09-15T22:39:45.046Z"
+// updated_at(pin): "2017-09-15T22:39:45.046Z"
+// story_id(pin): 1
+// landmark_id(pin): 1
+// image_url(pin): "https://memorytrail.s3.amazonaws.com/DG2015-san-francisco_1505515179102"
+// profile_display(pin): "Talis Lazdins"
