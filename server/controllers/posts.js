7173774df5d0ebfa3821bcdfc184b5ee1907151c
Kevin Huang <kevinzhuang10@gmail.com>
2017-09-15 20:21:23 -0700

    remove sentiment analysis

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,6 @@
 const models = require('../../db/models');
 const axios = require('axios');
-const NL_API = require('./natural_language_api.js');
+// const NL_API = require('./natural_language_api.js');
 
 module.exports.createPost = (req, res) => {
   var {post, landmark} = req.body;
@@ -14,12 +14,12 @@ module.exports.createPost = (req, res) => {
     .then(profile => {
       return profile.landmarks().attach(landmark_id);
     })
-    .then(() => {
-      return NL_API(post.content);
-    })
+    // .then(() => {
+    //   return NL_API(post.content);
+    // })
     .then(sentiment => {
-      post.score = sentiment.score;
-      post.magnitude = sentiment.magnitude;
+      // post.score = sentiment.score;
+      // post.magnitude = sentiment.magnitude;
       return models.Post.createPost(post);
     })
     .then(result => {
