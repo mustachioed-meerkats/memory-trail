0aed663d06742773d7f2327f4ad7dfe962a58287
Samuel Davis <davisss64@gmail.com>
2017-09-17 20:25:47 -0700

    Finished cleaning up create new post page.

---
+++ client/src/components/main/Upload.jsx
@@ -0,0 +1,89 @@
+import React from'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Button, Form, Header, Icon, Input } from 'semantic-ui-react';
+
+import {
+  handleImageUrl
+} from '../../store/modules/newpost';
+
+class Upload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+      previewImage: null
+    };
+    this.handleFile = this.handleFile.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    axios.post('/upload', this.state.file)
+      .then( res => {
+        let image_url = res.data.Location;
+        this.props.handleImageUrl(image_url);
+      })
+      .catch(err => {
+        console.log('failed', err);
+      });
+  }
+
+  handleFile(e) {
+    this.imagePreview();
+    let formData = new FormData();
+    formData.append('file', e.target.files[0]);
+    this.setState({
+      file: formData
+    });
+  }
+
+  imagePreview() {
+    let file = this.refs.file.files[0];
+    let reader = new FileReader();
+    let url = reader.readAsDataURL(file);
+    reader.onloadend = function (e) {
+      this.setState({
+        previewImage: [reader.result],
+      });
+    }.bind(this);
+
+  }
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="hidden-new-file">
+          <Icon name='photo' size='massive' color='teal' style={{cursor: 'pointer'}}/>
+        </label>
+        <input ref='file' type='file' id="hidden-new-file" style={{display: 'none'}} onChange={this.handleFile}/>
+        <Button size='small' onClick={this.handleSubmit}>Upload</Button>
+        <div>
+        <img src={this.state.previewImage} />
+      </div>
+      </div>
+    );
+  }
+};
+
+/** ============================================================
+ * Define State Subscriptions
+ * =============================================================
+ */
+const mapStateToProps = state => ({
+  image_url: state.newpost.image_url
+});
+
+/** ============================================================
+ * Define Dispatches Subscriptions
+ * =============================================================
+ */
+const mapDispatchToProps = dispatch => bindActionCreators({
+  handleImageUrl: handleImageUrl
+}, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Upload);
---
+++ client/src/store/modules/newpost.js
@@ -0,0 +1,164 @@
+import axios from 'axios';
+import { routerMiddleware, push } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+import {createStore} from 'redux';
+import thunk from 'redux-thunk';
+
+/** ============================================================
+ * Define Actions
+ * =============================================================
+ */
+export const SET_NEW_POST_TITLE_INPUT = 'newpost/SET_NEW_POST_TITLE_INPUT';
+export const SET_NEW_POST_CONTENT_TEXTAREA = 'newpost/SET_NEW_POST_CONTENT_TEXTAREA';
+export const SET_NEW_POST_LOCATION_INPUT = 'newpost/SET_NEW_POST_LOCATION_INPUT';
+export const SET_NEW_POST_IMAGEURL = 'newpost/SET_NEW_POST_IMAGEURL';
+export const HANDLE_STORY_LOAD = 'newpost/HANDLE_STORY_LOAD';
+export const HANDLE_NEW_POST = 'newpost/HANDLE_NEW_POST';
+/** ============================================================
+ * Define Initial State
+ * =============================================================
+ */
+const initialState = {
+  title: '',
+  content: '',
+  location: '',
+  story: '',
+  landmark: '',
+  profile_id: __PRELOADED_STATE__.user.user.id,
+  allUserStories: [{Test: 'Data'}],
+  image_url: '',
+};
+
+//The middleware below allows us to redirect from the redux store. 
+const middleware = routerMiddleware(browserHistory);
+
+
+/** ============================================================
+ * Define Reducer
+ * =============================================================
+ */
+export default (state = initialState, action) => {
+  switch (action.type) {
+  case SET_NEW_POST_TITLE_INPUT : 
+    return {
+      ...state,
+      title: action.title,
+      content: state.content,
+      location: state.location,
+      image_url: state.image_url
+    };
+  case SET_NEW_POST_CONTENT_TEXTAREA : 
+    return ({
+      ...state,
+      title: state.title,
+      content: action.content,
+      location: state.location,
+      image_url: state.image_url
+    });
+  case SET_NEW_POST_LOCATION_INPUT : 
+    return ({
+      ...state,
+      title: state.title,
+      content: state.content,
+      location: action.location,
+      image_url: state.image_url
+    });
+  
+  case SET_NEW_POST_IMAGEURL :
+    return ({
+      ...state,
+      title: state.title,
+      content: state.content,
+      location: state.location,
+      image_url: action.image_url
+    });
+  case HANDLE_NEW_POST : 
+    return ({
+      ...state,
+    });
+  case HANDLE_STORY_LOAD :
+    return ({
+      ...state,
+      allUserStories: action.allUserStories,
+    });
+    applyMiddleware(middleware);
+  default:
+    return state;
+  }
+};
+
+/** ============================================================
+ * Action Creators
+ * =============================================================
+ */
+
+export const handleTitleInput = (title) => {
+  return {
+    type: SET_NEW_POST_TITLE_INPUT,
+    title
+  };
+};
+
+export const handleContentTextArea = (content) => {
+  return {
+    type: SET_NEW_POST_CONTENT_TEXTAREA,
+    content
+  };
+};
+
+export const handleLocationInput = (location) => {
+  return {
+    type: SET_NEW_POST_LOCATION_INPUT,
+    location
+  };
+};
+
+export const handleStoryLoad = () => {
+  return dispatch => {
+    return loadStoriesByUser(initialState.profile_id)
+      .then(results => {
+        dispatch({
+          type: HANDLE_STORY_LOAD,
+          allUserStories: results.data,
+        });
+      });
+  };
+};
+
+
+export const handleNewPost = (postObject) => {
+  return (dispatch) => {
+    dispatch({
+      type: HANDLE_NEW_POST,
+    });
+    createNewPost(postObject)
+    .then(() => {
+      dispatch(push(`/profile/${initialState.profile_id}`));
+    })
+      .catch((err) => {
+      });
+  };
+};
+
+export const handleImageUrl = (image_url) => {
+  return {
+    type: SET_NEW_POST_IMAGEURL,
+    image_url
+  };
+};
+
+
+/**
+ * ==========================================================
+ * API Calls
+ * ==========================================================
+ */
+
+
+export const loadStoriesByUser = (profile_id) => {
+  return axios.get(`/api/stories/user/${profile_id}`);
+};
+
+export const createNewPost = (postObject) => {
+  return axios.post('/api/posts/new', postObject);
+};
