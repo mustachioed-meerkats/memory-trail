64e6fe5a7076d024b20674d2db8b4a2caa83a28a
dylan <dylan@dylans-MacBook-Pro.local>
2017-09-19 19:26:44 -0700

    Refactor passport list

--- client/src/components/main/ProfileRouterPage.jsx
+++ client/src/components/main/ProfileRouterPage.jsx
@@ -128,12 +128,8 @@ class ProfileRouterPage extends React.Component {
     var followButton = (<span></span>);
     var passportData;
     if (this.state.isCurrentUser) {
-<<<<<<< HEAD
       passportData = this.props.user.passport;
-      followingLink = (<li><Link to={`${this.props.match.url}/following`}>Following</Link></li>);
-=======
       followingLink = (<Link to={`${this.props.match.url}/following`}>Following</Link>);
->>>>>>> Integrate Timeline into Profile Page
     } else {
       passportData = this.props.otherUser.passport;
       if (this.state.isFollowing) {
@@ -142,13 +138,10 @@ class ProfileRouterPage extends React.Component {
         followButton = (<Button onClick={this.handleFollow}>Follow</Button>);
       }
     }
-    var passport = (
-      <div>
-        {passportData.map((passportEntry, index) => {
-          return (<div key={index}>{passportEntry.name}</div>);
-        })}
-      </div>
-    );
+
+    let passport = [... new Set(passportData.map((passportEntry, index) => {
+        return passportEntry.name;
+    }))];
 
     return (
       <Container fluid={true}>
@@ -158,7 +151,7 @@ class ProfileRouterPage extends React.Component {
             {this.state.profile_display}
             {followButton}
           </div>
-          <Menu text vertical>
+          <Menu text vertical size='large'>
             <Menu.Item>
               <Link to={`${this.props.match.url}`}>Stories</Link>
             </Menu.Item>
@@ -169,6 +162,17 @@ class ProfileRouterPage extends React.Component {
               {followingLink}
             </Menu.Item>
           </Menu>
+          <Divider/>
+          <List relaxed size='large'>
+            <List.Item>
+              <List.Header>Passport</List.Header>
+            </List.Item>
+            <List.Content>
+              {passport.map((place, index) => {
+                return <List.Item key={index}>{place}</List.Item>
+              })}
+            </List.Content>
+          </List>
           </Grid.Column>
           <Grid.Column width={14}>
             <Route exact path={`${this.props.match.url}`} render={() => (
