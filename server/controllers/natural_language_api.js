5837986243a46398d532d2f8e5478ec7a4d67bba
Kevin Huang <kevinzhuang10@gmail.com>
2017-09-19 23:59:19 -0700

    add cred to nl api

--- server/controllers/natural_language_api.js
+++ server/controllers/natural_language_api.js
@@ -2,7 +2,28 @@
 const Language = require('@google-cloud/language');
 
 // Instantiates a client
-const language = Language();
+// const language = Language();
+
+
+var env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  var language = Language();
+} else {
+  var language = Language({
+    credentials: {
+      "type": process.env.type || null,
+      "project_id": process.env.project_id || null,
+      "private_key_id": process.env.private_key_id || null,
+      "private_key": process.env.private_key.replace(/\\n/g, '\n') || null,
+      "client_email": process.env.client_email || null,
+      "client_id": process.env.client_id || null,
+      "auth_uri": process.env.auth_uri || null,
+      "token_uri": process.env.token_uri || null,
+      "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url || null,
+      "client_x509_cert_url": process.env.client_x509_cert_url || null
+    }
+  });
+}
 
 
 module.exports = (text) => {
