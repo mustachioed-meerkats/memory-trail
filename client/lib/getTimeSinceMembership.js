b34ae04bffb110d4975e578d8ca34bb57950b3ad
Kevin Huang <kevinzhuang10@gmail.com>
2017-09-21 11:25:00 -0700

    get rid of space

--- client/lib/getTimeSinceMembership.js
+++ client/lib/getTimeSinceMembership.js
@@ -1,4 +1,4 @@
-const getTimeSinceMembership = function (date) {
+const getTimeSinceMembership = function(date) {
   var objDate = new Date(date);
   var month = objDate.toLocaleString('en-us', { month: 'short' });
   var year = objDate.getFullYear();
